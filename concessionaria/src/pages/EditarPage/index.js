01ce96840901d14dbf7ab0739d9cbb4fba4a7618
Jhonatan Jacinto <jhonatanjacinto@gmail.com>
2020-12-10 09:19:09 -0300

    Navegação completa - Concessionaria

--- concessionaria/src/pages/EditarPage/index.js
+++ concessionaria/src/pages/EditarPage/index.js
@@ -1,8 +1,64 @@
+import MasterPage from "../../components/MasterPage/index.js";
+
 export default function EditarPage()
 {
     return (
-        <div>
-            Página de Edição
-        </div>
+        <MasterPage>
+            <form id="formVeiculo" method="POST" className="row">
+                <div className="form-group col-md-6">
+                    <label>Marca:</label>
+                    <select id="marca" className="form-control custom-select">
+                        <option value="">-- Selecionar --</option>
+                        <option value="Chevrolet">Chevrolet</option>
+                        <option value="Ford">Ford</option>
+                        <option value="Hyiundai">Hyiundai</option>
+                    </select>
+                    <div className="alert-danger w-100 p-2 d-none">Marca é obrigatório</div>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Modelo:</label>
+                    <input type="text" id="modelo" className="form-control" value="" placeholder="Insira o nome do modelo" />
+                    <div className="alert-danger w-100 p-2 d-none">Modelo inválido</div>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Ano:</label>
+                    <input type="number" id="ano" className="form-control" value="" placeholder="Insira o ano do modelo" />
+                    <div className="alert-danger w-100 p-2 d-none">Ano inválido</div>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Preço:</label>
+                    <input type="text" id="preco" className="form-control" value="" placeholder="Insira o preço do modelo" />
+                    <div className="alert-danger w-100 p-2 d-none">Preço inválido</div>
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Foto:</label>
+                    <input type="text" id="foto" className="form-control" value="" placeholder="Insira o nome da foto" />
+                </div>
+                <div className="form-group col-md-6">
+                    <label>Cor:</label>
+                    <select id="cor" className="form-control custom-select">
+                        <option value="">-- Selecionar --</option>
+                        <option value="Preto">Preto</option>
+                        <option value="Branco">Branco</option>
+                        <option value="Prata">Prata</option>
+                        <option value="Vermelho">Vermelho</option>
+                    </select>
+                    <div className="alert-danger w-100 p-2 d-none">Cor é obrigatório</div>
+                </div>
+                <div className="form-group col-md-12">
+                    <label>Descrição:</label>
+                    <textarea className="form-control" id="descricao" rows="10" placeholder="Insira a descrição do veículo"></textarea>
+                    <div className="alert-danger w-100 p-2 d-none">Descrição é obrigatório</div>
+                </div>
+                <div className="form-group col-md-12 text-right">
+                    <button className="btn btn-primary">
+                        Salvar Veículo
+                    </button>
+                    <button type="reset" className="btn btn-secondary">
+                        Limpar
+                    </button>
+                </div>
+            </form>
+        </MasterPage>
     );
 }
--- concessionaria/src/pages/HomePage/index.js
+++ concessionaria/src/pages/HomePage/index.js
@@ -4,11 +4,11 @@ export default function HomePage()
 {
     return (
         <MasterPage>
-            <section class="card">
-                <header class="card-header p-3">
+            <section className="card">
+                <header className="card-header p-3">
                     <h2>Bem-vindo(a) à nossa concessionária!</h2>
                 </header>
-                <div class="card-body p-3">
+                <div className="card-body p-3">
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem maiores laudantium facilis, rerum consequuntur minima possimus eum quod harum officia vitae voluptatem enim suscipit atque, earum ex sit dolorum illo!</p>
                 </div>
             </section>
